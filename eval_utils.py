a7e1eac66777eaef7d245ede51b5f4c0cb0173c8
wanglolly <wanglolly@gmail.com>
2018-04-26 01:39:12 +0800

    Debug message for Old Model

--- eval_utils.py
+++ eval_utils.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from torch import *
+import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
